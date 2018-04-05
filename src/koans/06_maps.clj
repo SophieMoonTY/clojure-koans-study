c0f92a8a77ac2824cc8ef7132ebb7ae2ad036dc9
SophieMoon <tae.moon.young@gmail.com>
2018-04-05 22:59:12 +0900

    Maps. 추가할 땐 "이것도 넣어야 함"

--- src/koans/06_maps.clj
+++ src/koans/06_maps.clj
@@ -33,19 +33,25 @@
   (= true (contains? {:a nil :b nil} :b))
 
   "Or if it is missing"
-  (= __ (contains? {:a nil :b nil} :c))
+  (= false (contains? {:a nil :b nil} :c))
 
   "Maps are immutable, but you can create a new and improved version"
-  (= {1 "January" 2 __} (assoc {1 "January"} 2 "February"))
+  (= {1 "January" 2 "February"} (assoc {1 "January"} 2 "February"))
 
   "You can also create a new version with an entry removed"
-  (= {__ __} (dissoc {1 "January" 2 "February"} 2))
+  (= {1 "January"} (dissoc {1 "January" 2 "February"} 2))
 
   "Create a new map by merging"
-  (= {:a 1 :b 2 __ __} (merge {:a 1 :b 2} {:c 3}))
+  (= {:a 1 :b 2 :c 3 } (merge {:a 1 :b 2} {:c 3}))
 
   "Specify how to handle entries with same keys when merging"
-  (= {:a 1 :b __ :c 3} (merge-with + {:a 1 :b 1} {:b 1 :c 3}))
+  (= {:a 1 :b 2 :c 3} (merge-with + {:a 1 :b 1} {:b 1 :c 3}))
+
+  "Specify how to handle entries with same keys when merging"
+  (= {:a 1 :b 2 :c 3} (merge-with - {:a 1 :b 5} {:b 3 :c 3}))
+
+  "Specify how to handle entries with same keys when merging"
+  (= {:a 1 :b 15 :c 3} (merge-with * {:a 1 :b 5} {:b 3 :c 3}))
 
   "Often you will need to get the keys, but the order is undependable"
   (= (list __ __ __)
