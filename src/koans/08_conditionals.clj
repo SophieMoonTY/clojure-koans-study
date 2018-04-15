7eb8499462c72412ec45c10dd253bd6890482c36
SophieMoon <tae.moon.young@gmail.com>
2018-04-15 17:11:52 +0900

    Condition base

--- src/koans/08_conditionals.clj
+++ src/koans/08_conditionals.clj
@@ -10,22 +10,16 @@
 
 (meditations
   "You will face many decisions"
-  (= __ (if (false? (= 4 5))
-          :a
-          :b))
+  (= :a (if (false? (= 4 5)) :a :b))
 
   "Some of them leave you no alternative"
-  (= __ (if (> 4 3)
-          []))
+  (= [] (if (> 4 3) []))
 
   "And in such a situation you may have nothing"
-  (= __ (if (nil? 0)
-          [:a :b :c]))
+  (= nil (if (nil? 0) [:a :b :c]))
 
   "In others your alternative may be interesting"
-  (= :glory (if (not (empty? ()))
-              :doom
-              __))
+  (= :glory (if (not (empty? ())) :doom :glory))
 
   "You may have a multitude of possible paths"
   (let [x 5]
