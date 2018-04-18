6337aba3df974c289673ca3d80c2cc2c47eed68e
SophieMoon <tae.moon.young@gmail.com>
2018-04-18 22:01:21 +0900

    Map, filter

--- src/koans/09_higher_order_functions.clj
+++ src/koans/09_higher_order_functions.clj
@@ -6,19 +6,19 @@
   (= [4 8 12] (map (fn [x] (* 4 x)) '(1 2 3)))
 
   "You may create that mapping"
-  (= [1 4 9 16 25] (map (fn [x] __) [1 2 3 4 5]))
+  (= [1 4 9 16 25] (map (fn [x] (* x x)) [1 2 3 4 5]))
 
   "Or use the names of existing functions"
-  (= __ (map nil? [:a :b nil :c :d]))
+  (= [false false true false false] (map nil? [:a :b nil :c :d]))
 
   "A filter can be strong"
-  (= __ (filter (fn [x] false) '(:anything :goes :here)))
+  (= [ ] (filter (fn [x] false) '(:anything :goes :here)))
 
   "Or very weak"
-  (= __ (filter (fn [x] true) '(:anything :goes :here)))
+  (= [:anything :goes :here] (filter (fn [x] true) '(:anything :goes :here)))
 
   "Or somewhere in between"
-  (= [10 20 30] (filter (fn [x] __) [10 20 30 40 50 60 70 80]))
+  (= [10 20 30] (filter (fn [x](< x 31)) [10 20 30 40 50 60 70 80]))
 
   "Maps and filters may be combined"
   (= [10 20 30] (map (fn [x] __) (filter (fn [x] __) [1 2 3 4 5 6 7 8])))
